cc3e78866fab5a66bca6f6d53649d2f99b478fc7
Miguel Ramírez <miguel03rf@gmail.com>
2022-10-13 00:57:35 -0500

    función con teclado completa

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // contiene la operacion o resultado parcial
 let parcial = "";
 
@@ -51,34 +50,68 @@ function operacion(oper) {
 // realizo el calculo de la formula matemática cuando presiona =
 function calculo() {
   // con eval evaluo la forula matemática que esta en formato string
-    parcial = eval(parcial);
-    txtresul.innerHTML = parcial;
-    // vuelvo a convertir en string porsi continua la formula
-    parcial = parcial.toString();
-    numero = "";
-
-    // operrealizado.innerHTML = parcial;
+  if(parcial == ""){
+      txtresul.innerHTML = 0;
+  }else{
+  parcial = eval(parcial);
+  txtresul.innerHTML = parcial;
+  // vuelvo a convertir en string porsi continua la formula
+  parcial = parcial.toString();
+  numero = "";
+  }
+  // operrealizado.innerHTML = parcial;
 }
 
 // funcion que limpia todo
 function limpiar() {
-operadorseleccionado = "";
-parcial = "";
-txtresul.innerHTML = "";
-numero = "";
-operrealizado.innerHTML = 0;
+  operadorseleccionado = "";
+  parcial = "";
+  txtresul.innerHTML = "";
+  numero = "";
+  operrealizado.innerHTML = 0;
 }
 
-function raizcuadrada(){
+// Obtener raíz cuadrada
+function raizcuadrada() {
   let raiz = Math.sqrt(parcial);
   txtresul.innerHTML = raiz;
 }
 
-const potencia = function(base, exponente) {
-  let resultado = 1;
-  for (let cuenta = 0; cuenta < exponente; cuenta++) {
-  resultado *= base;
+// Obtener valores del teclado y bloqueo de teclas 
+document.addEventListener("keydown", teclapresionada)
+
+function teclapresionada(event) {
+  let key = event.keyCode;
+  // validación de números
+  if (key >= 96 && key <= 105 || key >= 48 && key <= 57 || key > 109 && key < 111) {
+    operador(event.key);
   }
-  return resultado;
-  };
-  
+  // validación de operadores por teclado
+  if (key > 110 && key < 112 || key >= 106 && key <= 107 || key == '190') {
+    operacion(event.key);
+  }
+  // Realizar operación con tecla enter
+  if (key == '13') {
+    calculo();
+    getElementById("igual").focus();
+  }
+  //limpiado de pantalla por medio de la tecla delete
+  if (key == '8') {
+    // let borrar = parcial.slice(0,-1);
+    // operrealizado.innerHTML = borrar;
+    limpiar();
+  }
+}
+
+const potencia = function () {
+  let base = parseInt(parcial);
+  let resultado = 1;
+  for (let cuenta = 0; cuenta < 2; cuenta++) {
+    resultado *= base;
+  }
+  if (!Number.isNaN(resultado)) {
+    txtresul.innerHTML = resultado;
+  } else {
+    txtresul.innerHTML = 0;
+  }
+};
